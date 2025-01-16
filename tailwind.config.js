f65730f443ef5ecb96234bf5dafbab74128608b3
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2025-01-17 00:28:55 +0200

    The Project is Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,6 @@ module.exports = {
       screens:{
         '2md': '960px'
       },
-      colors: { 
-        'blueText': '#181E4B',
-        'orangeText': '#DF6951'
-      },
       fontFamily: {
         nanoSans: ['Nano Sans', 'sans-serif'],
         Poppins: ['Poppins', 'sans-serif'],
